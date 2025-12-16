28347b5fd2a9540c116f9418bc83319c84fdb34a
Morteza-hosseinzadeh <morteza.hoseinzadeh553@rahaco.com>
2025-12-16 18:43:06 +0330

    fix issues

--- src/components/pages/Home/CoopiesLogoSection.tsx
+++ src/components/pages/Home/CoopiesLogoSection.tsx
@@ -13,7 +13,7 @@ export default function CoopiesLogoSection() {
 
   return (
     <Box display="flex" alignItems="center" justifyContent="space-between" flexWrap="wrap" gap={{ xs: 2, sm: 4, md: 6 }} px={{ xs: 2, md: 6 }} py={{ xs: 4, md: 6 }}>
-      {imgs.map((img, idx) => (
+      {imgs?.map((img, idx) => (
         <Tooltip key={idx} title={img.tooltip} arrow>
           <Box flex="0 1 115px" display="flex" justifyContent="center" alignItems="center" sx={{ img: { ...styles.img } }}>
             <Image src={`/assets/logo/company-logo/${img.src}.png`} alt={img.src} width={idx > 2 ? 65 : 85} height={idx > 2 ? 65 : 85} priority />
@@ -30,8 +30,6 @@ const styles = {
     filter: 'saturate(0)',
     transition: 'all ease-in-out 0.3s',
     cursor: 'pointer',
-    '&:hover': {
-      filter: 'saturate(1)',
-    },
+    '&:hover': { filter: 'saturate(1)' },
   },
 };
