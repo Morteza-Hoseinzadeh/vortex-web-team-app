6d65334f3d5642528835e9ed329c59b8beefceaa
Morteza-hosseinzadeh <morteza.hoseinzadeh553@rahaco.com>
2025-12-17 21:18:33 +0330

    Develop pricing table section with responsive mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import OurServices from '@/components/pages/Home/OurServices';
 import CoopiesLogoSection from '@/components/pages/Home/CoopiesLogoSection';
 import Portfolios from '@/components/pages/Home/Portfolios';
 import CoopWays from '@/components/pages/Home/CoopWays';
+import PricingTable from '@/components/pages/Home/PricingTable';
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
         <CoopiesLogoSection />
         <Portfolios />
         <CoopWays />
+        <PricingTable />
       </Box>
     </ChildrenLayout>
   );
